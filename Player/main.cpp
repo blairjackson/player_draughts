56ca9580f94ec0627e9d55c57867dfea109a4956
blairjackson <blairgjackson@gmail.com>
2018-05-09 09:14:13 +1000

    player files first commit

---
+++ Player/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: blairjackson
+ *
+ * Created on 9 May 2018, 9:13 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
